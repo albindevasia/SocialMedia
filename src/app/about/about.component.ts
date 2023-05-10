68d28f756a6fe241aaca6564ea9be15b6ca00f3f
albindevasia <114718268+albindevasia@users.noreply.github.com>
2023-05-10 09:02:15 +0530

    s

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,7 +6,7 @@ import { ApiService } from '../Services/friends.profile';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+
 })
 export class AboutComponent {
   username: any;
